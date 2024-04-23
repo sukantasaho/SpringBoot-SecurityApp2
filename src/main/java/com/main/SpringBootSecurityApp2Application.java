c544f45cb7258fca24f6ee2963c1ea3efdd98a18
sukantasaho <ssukanta520@gmail.com>
2024-04-23 18:47:57 +0530

    commit

---
+++ src/main/java/com/main/SpringBootSecurityApp2Application.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityApp2Application.class, args);
+	}
+
+}
